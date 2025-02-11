ef1aa7561186a9c26afdbf53e7aa4ec54dc577c9
Julian Johari <julianjohari02@gmail.com>
2025-02-12 01:44:44 +0800

    Tailwind Css Setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awards</h1>
+      <h1 className='text-5xl font-bold text-violet-300'>Welcome to Awards</h1>
     </main>
   )
 }
